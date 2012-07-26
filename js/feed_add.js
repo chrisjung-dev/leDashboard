06b30f75f236da5c8481fb0f41f0ef3430bce591
Chris Jung <campino2k@gmail.com>
2012-07-26 07:28:02 +0200

    Refactoring, Cleanup for future improvements

--- js/feed_add.js
+++ js/feed_add.js
@@ -7,9 +7,10 @@ open_add_feed_form = function() {
 }
 
 var id = $( "#id" ),
-	email = $( "#email" ),
+	site_title = $( "#site_title" ),
+
 	password = $( "#password" ),
-	allFields = $( [] ).add( name ).add( email ).add( password ),
+	allFields = $( [] ).add( id ).add( site_title ).add( password ),
 	tips = $( ".validateTips" );
 
 function updateTips( t ) {
@@ -49,32 +50,32 @@ $( "#new-feed-form" ).dialog({
 	modal: true,
 	buttons: {
 		"Add this Feed": function() {
-		var bValid = true;
-		allFields.removeClass( "ui-state-error" );
+			var bValid = true;
+			allFields.removeClass( "ui-state-error" );
 
-		bValid = bValid && checkRegexp( id, /^([0-9a-zA-Z_-])+$/, "ID field only allows : a-z 0-9 _ -" );
+			bValid = bValid && checkRegexp( id, /^[a-zA-Z]([0-9a-zA-Z_-])+$/, "ID field only allows : a-z 0-9 _ -, must begin with a letter." );
 
-		bValid = bValid && checkLength( name, "username", 3, 16 );
-		bValid = bValid && checkLength( email, "email", 6, 80 );
-		bValid = bValid && checkLength( password, "password", 5, 16 );
+	//		bValid = bValid && checkLength( site_title, "username", 3, 16 );
+	//		bValid = bValid && checkLength( email, "email", 6, 80 );
+	//		bValid = bValid && checkLength( password, "password", 5, 16 );
 
-		bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-		// From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-		bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+	//		bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
 
-if ( bValid ) {
+			if ( bValid ) {
 
-	/**
-	 *	Here, add the new feed to feeds var, render and save!
-	 */ 
-	$( this ).dialog( "close" );
-}
-},
-Cancel: function() {
-	$( this ).dialog( "close" );
-}
-},
-close: function() {
-allFields.val( "" ).removeClass( "ui-state-error" );
-}
+				/**
+				 *	Here, add the new feed to feeds var, render and save!
+				 */ 
+				$( this ).dialog( "close" );
+			}
+		},
+		Cancel: function() {
+			$( this ).dialog( "close" );
+		}
+	},
+	close: function() {
+		// TODO
+		// Create the new Feed.
+		allFields.val( "" ).removeClass( "ui-state-error" );
+	}
 });
