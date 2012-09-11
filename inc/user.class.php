b064b8e22f5ea52d82a83988dd1c191c6dcf380b
Chris Jung <campino2k@gmail.com>
2012-09-11 08:26:19 +0200

    Add get_user_session

--- inc/user.class.php
+++ inc/user.class.php
@@ -1,9 +1,19 @@
 <?php
 
 class User {
+
 	function login() {
 	}
 
+	/**
+	 *	Returns the valid Session or false if there is no Session found 
+	 *
+	 * @author Chris Jung <campino2k@gmail.com>
+	 * @since 2012.09.11
+	 */
+	function get_user_session() {
+	}
+	
 	function logout() {
 	}
 }
