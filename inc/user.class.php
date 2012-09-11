61f4f423857835288075842804d9afdae45ac85a
Chris Jung <campino2k@gmail.com>
2012-09-11 19:34:14 +0200

    Add user behaviour

--- inc/user.class.php
+++ inc/user.class.php
@@ -2,14 +2,14 @@
 
 class User {
 
-	function login( _username, _password ) {
+	public function login( _username; _password ) {
 		// get JSON File, Encode, 
 		$users = '';
 		
 		session_start();
 		$_SESSION[ 'username ' ] = 'cjung';
 
-	}
+	};
 
 	/**
 	 *	Returns the valid Session or false if there is no Session found 
@@ -17,20 +17,21 @@ class User {
 	 * @author Chris Jung <campino2k@gmail.com>
 	 * @since 2012.09.11
 	 */
-	function get_user_session() {
+	public function get_user_session() {
 
-		if( isset( $_SESSION[ 'username' ] ) :
+		if ( isset( $_SESSION[ 'username' ] ) ) :
 			
 			$user_name = $_SESSION[ 'username ' ];
 			return $user_name;
 		
-		: else :
+		else :
 			
 			return false;
 
-		: endif
-	}
+		endif;
+		
+	};
 	
-	function logout() {
-	}
+	public function logout() {
+	};
 }
