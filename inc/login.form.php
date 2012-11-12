099e3d5a12b71b623ff5eb40b7a9b250163ff61a
Chris Jung <campino2k@gmail.com>
2012-11-12 23:49:23 +0100

    Font Family better used

--- inc/login.form.php
+++ inc/login.form.php
@@ -15,3 +15,7 @@
 	</fieldset>
 
 </form>
+<footer>
+	<p>2012, <a href="http://campino2k.de">Christian Jung</a> | Fork me on <a href="http://github.com/campino2k/leDashboard">Github</a> | Dashboard Icon <a href="http://creativecommons.org/licenses/by/3.0/">CC-BY</a> from <a href="http://www.doublejdesign.co.uk/products-page/icons/super-mono-icons/">Double-J Design's "Super Mono" Iconset</a></p>
+</footer>
+					
