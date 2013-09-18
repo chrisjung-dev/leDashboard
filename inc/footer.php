eb16c134ea31d4f6a5c5c99da1cad2c3e55e907d
Chris Jung <campino2k@gmail.com>
2013-09-18 20:57:54 +0200

    Upgrade to newer jQuery / jQueryUI

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 			<p>2012, <a href="http://campino2k.de">Christian Jung</a> | Fork me on <a href="http://github.com/campino2k/leDashboard">Github</a> | Dashboard Icon <a href="http://creativecommons.org/licenses/by/3.0/">CC-BY</a> from <a href="http://www.doublejdesign.co.uk/products-page/icons/super-mono-icons/">Double-J Design's "Super Mono" Iconset</a></p>
 		</footer>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 
 	<script src="lib/underscore/underscore-min.js"></script>
--- inc/header.php
+++ inc/header.php
@@ -5,9 +5,8 @@
 		<title>leDashboard</title>
 		<link rel="shortcut icon" href="dist/img/favicon.ico" />
 
-		<!-- link rel="stylesheet" type="text/css" href="style/css/ledashboard/jquery-ui-1.8.21.custom.css" -->
-		<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css">
-		grunt
+		<link rel="stylesheet" type="text/css" href="dist/style/css/flick/jquery-ui-1.8.21.custom.css">
+		
 		<!-- CSS includes -->
 		<link rel="stylesheet" href="dist/style/screen.css" />
 
