f46fd04ac563f82c3e618fa818963f6b160d5650
Chris Jung <campino2k@gmail.com>
2014-01-14 22:18:04 +0100

    [TASK] First draft of MenuController

---
+++ src/js/controllers/MenuController.js
@@ -0,0 +1,13 @@
+/**
+ * src/js/controllers/MenuController.js
+ * @author Christian Jung <campino2k@gmail.com>
+ * @since 2013-12-29 12:17
+ */
+
+function MenuController( $scope, $http, $log, $element, $timeout, $interval ){
+
+	$scope.reloadAll = function(){
+
+	}
+}
+
